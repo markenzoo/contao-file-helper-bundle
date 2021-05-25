7e49435a2a513be10cf3870915f63e892e49c022
Felix Kästner <kaestner.fe@gmail.com>
2021-05-25 16:52:36 +0200

    refactor service configuration

---
+++ src/DependencyInjection/ContaoFileHelperExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of markenzoo/contao-file-helper-bundle.
+ *
+ * Copyright (c) 2021 markenzoo eG
+ *
+ * @package   markenzoo/contao-file-helper-bundle
+ * @author    Felix Kästner <kaestner@markenzoo.de>
+ * @copyright 2021 markenzoo eG
+ * @license   https://github.com/markenzoo/contao-file-helper-bundle/blob/master/LICENSE MIT License
+ */
+
+namespace Markenzoo\ContaoFileHelperBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ContaoFileHelperExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('services.yml');
+    }
+}
